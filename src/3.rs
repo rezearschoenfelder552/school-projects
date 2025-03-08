158bb5be52d36e7d2714c5609d9d5606cad17511
rezearschoenfelder552 <rezearschoenfelder552@gmail.com>
2025-03-08 13:55:00 +0000

    Add 3.rs

---
+++ src/3.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    let mut x = 5;
+    let y = 10;
+
+    if x < y {
+        println!("{} is less than {}", x, y);
+    } else {
+        println!("{} is greater than or equal to {}", x, y);
+    }
+}
